00b5cf3cbec398e5801fa3822d32558bfa3e7e8f
shalinimalipatil <shalinideshmukh1991@gmail.com>
2017-05-08 01:04:18 -0700

    hellooo

---
+++ db/migrate/20170508080359_create_deshes.rb
@@ -0,0 +1,10 @@
+class CreateDeshes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :deshes do |t|
+      t.string :name
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
