da37bd254bd986ab60c031aff01d28251b3e3d2c
p-bhargavi <paturibhargavi.123@gmail.com>
2017-05-08 01:05:04 -0700

    hello

---
+++ db/migrate/20170508080419_create_bhargavis.rb
@@ -0,0 +1,10 @@
+class CreateBhargavis < ActiveRecord::Migration[5.0]
+  def change
+    create_table :bhargavis do |t|
+      t.string :name
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
